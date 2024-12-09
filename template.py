2af7efd4f28e74636d6ecf2dde3fffd255789450
Hefaj Uddin <hefaj@ecosoftbd.com>
2024-12-09 11:33:32 +0600

    create all necessary files

---
+++ template.py
@@ -0,0 +1,30 @@
+import os, sys
+from pathlib import Path
+import logging
+
+listOfFiles=[
+    "model.py",
+    "trainer.py",
+    f"templates/index.html",
+    f"data/context.py",
+    f"data/dataset.py",
+    f"data/collator.py",
+    ".env",
+    "config.py",
+    "requirements.txt"
+]
+
+for path in listOfFiles:
+    filepath=Path(path)
+    filedir, filename=os.path.split(path)
+
+    if filedir!="":
+        os.makedirs(filedir, exist_ok=True)
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass
+
+    else:
+        logging.info("file is already present at :{filepath}" )
+
